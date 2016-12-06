531bf55e2fd2dedb26e31dfd2113a6aa8d43ed70
NicolasDEROUINEAU <nicolas.derouineau@telecom-paristech.fr>
2016-12-06 16:27:29 +0100

    First working draft

--- demuxing_decoding_filtering.c
+++ demuxing_decoding_filtering.c
@@ -34,8 +34,11 @@
 #include <libavutil/timestamp.h>
 #include <libavformat/avformat.h>
 
+#define AVCODEC_MAX_AUDIO_FRAME_SIZE 192000
+
 #include <ao/ao.h>
 #include <math.h>
+#include <limits.h>
 
 static AVFormatContext *fmt_ctx = NULL;
 static AVCodecContext *video_dec_ctx = NULL, *audio_dec_ctx;
@@ -57,6 +60,8 @@ static AVFrame *frame = NULL;
 static AVPacket pkt;
 static int video_frame_count = 0;
 static int audio_frame_count = 0;
+static int data_size=0;
+static int plane_size=0;
 
 /* Enable or disable frame reference counting. You are not supposed to support
  * both paths in your application but pick the one most appropriate to your
@@ -75,6 +80,12 @@ static int buf_size;
 static int sample;
 static float freq = 440.0;
 static int i;
+
+const int buffer_size=AVCODEC_MAX_AUDIO_FRAME_SIZE+ FF_INPUT_BUFFER_PADDING_SIZE;
+
+
+
+#define AVCODEC_MAX_AUDIO_FRAME_SIZE 192000
 
 
 static int decode_packet(int *got_frame, int cached)
@@ -125,79 +136,41 @@ static int decode_packet(int *got_frame, int cached)
         }
     } else if (pkt.stream_index == audio_stream_idx) {
         /* decode audio frame */
-//        switch(audio_dec_ctx->sample_fmt){
-//        case AV_SAMPLE_FMT_U8:
-//            printf("AV_SAMPLE_FMT_U8\n");
-//            break;
-//        case AV_SAMPLE_FMT_S16:
-//            printf("AV_SAMPLE_FMT_S16\n");
-//            break;
-//        case AV_SAMPLE_FMT_S32:
-//            printf("AV_SAMPLE_FMT_S32\n");
-//            break;
-//        case AV_SAMPLE_FMT_FLT:
-//            printf("AV_SAMPLE_FMT_FLT\n");
-//            break;
-//        case AV_SAMPLE_FMT_DBL:
-//            printf("AV_SAMPLE_FMT_DBL\n");
-//            break;
-//        case AV_SAMPLE_FMT_U8P:
-//            printf("AV_SAMPLE_FMT_U8P\n");
-//            break;
-//        case AV_SAMPLE_FMT_S16P:
-//            printf("AV_SAMPLE_FMT_S16P\n");
-//            break;
-//        case AV_SAMPLE_FMT_S32P:
-//            printf("AV_SAMPLE_FMT_S32P\n");
-//            break;
-//        case AV_SAMPLE_FMT_FLTP:
-//            printf("AV_SAMPLE_FMT_FLTP\n");
-//            break;
-//        default:
-//            printf("Wrong Sample format\n");
-//            break;
-//        }
 
         ret = avcodec_decode_audio4(audio_dec_ctx, frame, got_frame, &pkt);
         if (ret < 0) {
             fprintf(stderr, "Error decoding audio frame (%s)\n", av_err2str(ret));
             return ret;
         }
+
+        data_size = av_samples_get_buffer_size(&plane_size, audio_dec_ctx->channels,
+                                               frame->nb_samples,
+                                               audio_dec_ctx->sample_fmt, 1);
         /* Some audio decoders decode only part of the packet, and have to be
          * called again with the remainder of the packet data.
          * Sample: fate-suite/lossless-audio/luckynight-partial.shn
          * Also, some decoders might over-read the packet. */
         decoded = FFMIN(ret, pkt.size);
 
-
+        const uint8_t samples[buffer_size];
+        uint16_t *out = (uint16_t *)samples;
 
         if (*got_frame) {
-            /*AV_SAMPLE_FMT_U8,          ///< unsigned 8 bits
-            AV_SAMPLE_FMT_S16,         ///< signed 16 bits
-            AV_SAMPLE_FMT_S32,         ///< signed 32 bits
-            AV_SAMPLE_FMT_FLT,         ///< float
-            AV_SAMPLE_FMT_DBL,         ///< double
-
-            AV_SAMPLE_FMT_U8P,         ///< unsigned 8 bits, planar
-            AV_SAMPLE_FMT_S16P,        ///< signed 16 bits, planar
-            AV_SAMPLE_FMT_S32P,        ///< signed 32 bits, planar
-            AV_SAMPLE_FMT_FLTP,        ///< float, planar*/
-
-            size_t unpadded_linesize = frame->nb_samples * av_get_bytes_per_sample(frame->format);
-            /*printf("audio_frame%s n:%d nb_samples:%d pts:%s\n",
-                   cached ? "(cached)" : "",
-                   audio_frame_count++, frame->nb_samples,
-                   av_ts2timestr(frame->pts, &audio_dec_ctx->time_base));*/
-
-            /* Write the raw audio data samples of the first plane. This works
-             * fine for packed formats (e.g. AV_SAMPLE_FMT_S16). However,
-             * most audio decoders output planar audio, which uses a separate
-             * plane of audio samples for each channel (e.g. AV_SAMPLE_FMT_S16P).
-             * In other words, this code will write only the first audio channel
-             * in these cases.
-             * You should use libswresample or libavfilter to convert the frame
-             * to packed data. */
-            fwrite(frame->extended_data[0], 1, unpadded_linesize, audio_dst_file);
+            int write_p=0;
+            if(audio_dec_ctx->sample_fmt==AV_SAMPLE_FMT_FLTP){
+                int nb=0;
+                for (;nb<plane_size/sizeof(float);nb++){
+                    int ch = 0;
+                    for (; ch < audio_dec_ctx->channels; ch++) {
+                        out[write_p]  = ((float *) frame->extended_data[ch])[nb] * 32767.0f ;
+                        write_p++;
+                    }
+                }
+                printf("plane_size = %d \n",plane_size);
+                ao_play(device, (char*)samples, ( plane_size/sizeof(float) )* sizeof(uint16_t) * audio_dec_ctx->channels );
+            }
+            //size_t unpadded_linesize = frame->nb_samples * av_get_bytes_per_sample(frame->format);
+            //fwrite(frame->extended_data[0], 1, unpadded_linesize, audio_dst_file);
         }
     }
 
@@ -440,10 +413,24 @@ int main (int argc, char **argv)
     default_driver = ao_default_driver_id();
 
     memset(&ao_format, 0, sizeof(ao_format));
-    ao_format.bits = 16;
-    ao_format.channels = 1;
-    ao_format.rate = 44100;
-    ao_format.byte_format = AO_FMT_LITTLE;
+    if(audio_dec_ctx->sample_fmt==AV_SAMPLE_FMT_FLT || audio_dec_ctx->sample_fmt==AV_SAMPLE_FMT_FLTP) {
+        ao_format.bits = 16;
+        ao_format.channels = audio_dec_ctx->channels;
+        ao_format.rate = audio_dec_ctx->sample_rate;
+        ao_format.byte_format = AO_FMT_NATIVE;
+        ao_format.matrix=0;
+    }
+    else
+    {
+        exit(1);
+    }
+
+    /* -- Open driver -- */
+    device = ao_open_live(default_driver, &ao_format, NULL /* no options */);
+    if (device == NULL) {
+        fprintf(stderr, "Error opening device.\n");
+        return 1;
+    }
 
     /* read frames from the file */
     while (av_read_frame(fmt_ctx, &pkt) >= 0) {
