f857f8bcee3bbd7f8db734ece1a5c63655a3ebc3
NicolasDEROUINEAU <nicolas.derouineau@telecom-paristech.fr>
2017-04-20 15:00:15 +0200

    Fix wrong indentation

--- core/src/demuxing_decoding.cpp
+++ core/src/demuxing_decoding.cpp
@@ -12,35 +12,35 @@ DemuxDecode::DemuxDecode(const char* src_file_name, pthread_mutex_t *mutex, pthr
 
     /* open input file, and allocate format context */
     if (avformat_open_input(&fmt_ctx, m_src_filename, NULL, NULL) < 0) {
-    fprintf(stderr, "Could not open source file %s\n", m_src_filename);
-    exit(1);
+        fprintf(stderr, "Could not open source file %s\n", m_src_filename);
+        exit(1);
     }
 
     /* retrieve stream information */
     if (avformat_find_stream_info(fmt_ctx, NULL) < 0) {
-    fprintf(stderr, "Could not find stream information\n");
-    exit(1);
+        fprintf(stderr, "Could not find stream information\n");
+        exit(1);
     }
 
 
     if (open_codec_context(&audio_stream_idx, &audio_dec_ctx, fmt_ctx, AVMEDIA_TYPE_AUDIO) >= 0) {
-    audio_stream = fmt_ctx->streams[audio_stream_idx];
+        audio_stream = fmt_ctx->streams[audio_stream_idx];
     }
 
     /* dump input information to stderr */
     //av_dump_format(fmt_ctx, 0, m_src_filename, 0);
 
     if (!audio_stream) {
-    fprintf(stderr, "Could not find audio  stream in the input, aborting\n");
-    ret = 1;
-    //goto end;
+        fprintf(stderr, "Could not find audio  stream in the input, aborting\n");
+        ret = 1;
+        //goto end;
     }
 
     frame = av_frame_alloc();
     if (!frame) {
-    fprintf(stderr, "Could not allocate frame\n");
-    ret = AVERROR(ENOMEM);
-    //goto end;
+        fprintf(stderr, "Could not allocate frame\n");
+        ret = AVERROR(ENOMEM);
+        //goto end;
     }
 
     /* initialize packet, set data to NULL, let the demuxer fill it */
@@ -56,11 +56,11 @@ DemuxDecode::DemuxDecode(const char* src_file_name, pthread_mutex_t *mutex, pthr
 }
 
 DemuxDecode::~DemuxDecode(){
-        avcodec_free_context(&video_dec_ctx);
-        avcodec_free_context(&audio_dec_ctx);
-        avformat_close_input(&fmt_ctx);
-        av_frame_free(&frame);
-        av_free(video_dst_data[0]);
+    avcodec_free_context(&video_dec_ctx);
+    avcodec_free_context(&audio_dec_ctx);
+    avformat_close_input(&fmt_ctx);
+    av_frame_free(&frame);
+    av_free(video_dst_data[0]);
 }
 
 void DemuxDecode::InternalThreadEntry(){   
@@ -77,45 +77,45 @@ int DemuxDecode::open_codec_context(int *stream_idx, AVCodecContext **dec_ctx, A
 
     ret = av_find_best_stream(fmt_ctx, type, -1, -1, NULL, 0);
     if (ret < 0) {
-    fprintf(stderr, "Could not find %s stream in input file '%s'\n",
-    av_get_media_type_string(type), m_src_filename);
-    return ret;
+        fprintf(stderr, "Could not find %s stream in input file '%s'\n",
+                av_get_media_type_string(type), m_src_filename);
+        return ret;
     } else {
-    stream_index = ret;
-    st = fmt_ctx->streams[stream_index];
+        stream_index = ret;
+        st = fmt_ctx->streams[stream_index];
 
-    /* find decoder for the stream */
-    dec = avcodec_find_decoder(st->codecpar->codec_id);
-    if (!dec) {
-        fprintf(stderr, "Failed to find %s codec\n",
-        av_get_media_type_string(type));
-        return AVERROR(EINVAL);
-    }
+        /* find decoder for the stream */
+        dec = avcodec_find_decoder(st->codecpar->codec_id);
+        if (!dec) {
+            fprintf(stderr, "Failed to find %s codec\n",
+                    av_get_media_type_string(type));
+            return AVERROR(EINVAL);
+        }
 
-    /* Allocate a codec context for the decoder */
-    *dec_ctx = avcodec_alloc_context3(dec);
-    if (!*dec_ctx) {
-        fprintf(stderr, "Failed to allocate the %s codec context\n",
-        av_get_media_type_string(type));
-        return AVERROR(ENOMEM);
-    }
+        /* Allocate a codec context for the decoder */
+        *dec_ctx = avcodec_alloc_context3(dec);
+        if (!*dec_ctx) {
+            fprintf(stderr, "Failed to allocate the %s codec context\n",
+                    av_get_media_type_string(type));
+            return AVERROR(ENOMEM);
+        }
 
-    /* Copy codec parameters from input stream to output codec context */
-    if ((ret = avcodec_parameters_to_context(*dec_ctx, st->codecpar)) < 0) {
-        fprintf(stderr, "Failed to copy %s codec parameters to decoder context\n",
-        av_get_media_type_string(type));
-        return ret;
-    }
+        /* Copy codec parameters from input stream to output codec context */
+        if ((ret = avcodec_parameters_to_context(*dec_ctx, st->codecpar)) < 0) {
+            fprintf(stderr, "Failed to copy %s codec parameters to decoder context\n",
+                    av_get_media_type_string(type));
+            return ret;
+        }
 
 
-    /* Init the decoders, with or without reference counting */
-    av_dict_set(&opts, "refcounted_frames", refcount ? "1" : "0", 0);
-    if ((ret = avcodec_open2(*dec_ctx, dec, &opts)) < 0) {
-        fprintf(stderr, "Failed to open %s codec\n",
-        av_get_media_type_string(type));
-        return ret;
-    }
-    *stream_idx = stream_index;
+        /* Init the decoders, with or without reference counting */
+        av_dict_set(&opts, "refcounted_frames", refcount ? "1" : "0", 0);
+        if ((ret = avcodec_open2(*dec_ctx, dec, &opts)) < 0) {
+            fprintf(stderr, "Failed to open %s codec\n",
+                    av_get_media_type_string(type));
+            return ret;
+        }
+        *stream_idx = stream_index;
 
     }
 
@@ -132,52 +132,52 @@ void DemuxDecode::decode_packet(int *got_frame, int *bytes_read,int cached)
     int decoded = pkt.size;
 
     if (pkt.stream_index == audio_stream_idx) {
-    /* decode audio frame */
+        /* decode audio frame */
 
-    ret = avcodec_decode_audio4(audio_dec_ctx, frame, got_frame, &pkt);
-    if (ret < 0) {
-        /*fprintf(stderr, "Error decoding audio frame (%s)\n", av_err2str(ret));*/
-        //return ret;
-        *bytes_read=-1;
-        goto endofdecoding;
-    }
+        ret = avcodec_decode_audio4(audio_dec_ctx, frame, got_frame, &pkt);
+        if (ret < 0) {
+            /*fprintf(stderr, "Error decoding audio frame (%s)\n", av_err2str(ret));*/
+            //return ret;
+            *bytes_read=-1;
+            goto endofdecoding;
+        }
 
-    data_size = av_samples_get_buffer_size(&plane_size, audio_dec_ctx->channels,
-    frame->nb_samples,
-    audio_dec_ctx->sample_fmt, 1);
+        data_size = av_samples_get_buffer_size(&plane_size, audio_dec_ctx->channels,
+                                               frame->nb_samples,
+                                               audio_dec_ctx->sample_fmt, 1);
 
 
-    /* Some audio decoders decode only part of the packet, and have to be
+        /* Some audio decoders decode only part of the packet, and have to be
      * called again with the remainder of the packet data.
      * Sample: fate-suite/lossless-audio/luckynight-partial.shn
      * Also, some decoders might over-read the packet. */
-    decoded = FFMIN(ret, pkt.size);
+        decoded = FFMIN(ret, pkt.size);
 
-    uint8_t samples[buffer_size];
-    uint16_t *out = (uint16_t *)samples;
+        uint8_t samples[buffer_size];
+        uint16_t *out = (uint16_t *)samples;
 
-    if (*got_frame) {
-        if(audio_dec_ctx->sample_fmt==AV_SAMPLE_FMT_FLTP){
-        for (int j=0; j<audio_dec_ctx->channels; j++) {
-            float* inputChannel = (float*)frame->extended_data[j];
-            for (int i=0 ; i<frame->nb_samples ; i++) {
-            float sample = inputChannel[i];
-            if (sample<-1.0f) sample=-1.0f;
-            else if (sample>1.0f) sample=1.0f;
-            out[i*audio_dec_ctx->channels + j] = (int16_t) (sample * 32767.0f );
+        if (*got_frame) {
+            if(audio_dec_ctx->sample_fmt==AV_SAMPLE_FMT_FLTP){
+                for (int j=0; j<audio_dec_ctx->channels; j++) {
+                    float* inputChannel = (float*)frame->extended_data[j];
+                    for (int i=0 ; i<frame->nb_samples ; i++) {
+                        float sample = inputChannel[i];
+                        if (sample<-1.0f) sample=-1.0f;
+                        else if (sample>1.0f) sample=1.0f;
+                        out[i*audio_dec_ctx->channels + j] = (int16_t) (sample * 32767.0f );
+                    }
+                }
+                pthread_mutex_lock(m_mutex);
+                m_buffer->Write(samples, 2*audio_dec_ctx->channels*frame->nb_samples);
+                pthread_mutex_unlock(m_mutex);
+                pthread_cond_signal(m_signal);
             }
         }
-        pthread_mutex_lock(m_mutex);
-        m_buffer->Write(samples, 2*audio_dec_ctx->channels*frame->nb_samples);
-        pthread_mutex_unlock(m_mutex);
-        pthread_cond_signal(m_signal);
-        }
-    }
     }
 
 
     if(!(ret<0))
-    *bytes_read=ret;
+        *bytes_read=ret;
 endofdecoding:
     ;
 
@@ -188,33 +188,33 @@ void *DemuxDecode::decode_thread(void *x_void_ptr)
     int num_bytes=0,got_frame;
     int got_space;
     while (1) {
-    pthread_mutex_lock(m_mutex);
-    got_space=m_buffer->GetWriteAvail();
-    pthread_mutex_unlock(m_mutex);
-    //printf("got_space= %d",got_space );
-    if(got_space>5000){
-        if(av_read_frame(fmt_ctx, &pkt) >= 0){
-        AVPacket orig_pkt = pkt;
-        do {
-            decode_packet(&got_frame, &num_bytes,0);
-            if (num_bytes < 0)
-            break;
-            pkt.data += num_bytes;
-            pkt.size -= num_bytes;
-        } while (pkt.size > 0);
-        av_packet_unref(&orig_pkt);}
-        else
-        {
         pthread_mutex_lock(m_mutex);
-        m_ctx->state=END_OF_DECODING;
+        got_space=m_buffer->GetWriteAvail();
         pthread_mutex_unlock(m_mutex);
-        break;
+        //printf("got_space= %d",got_space );
+        if(got_space>5000){
+            if(av_read_frame(fmt_ctx, &pkt) >= 0){
+                AVPacket orig_pkt = pkt;
+                do {
+                    decode_packet(&got_frame, &num_bytes,0);
+                    if (num_bytes < 0)
+                        break;
+                    pkt.data += num_bytes;
+                    pkt.size -= num_bytes;
+                } while (pkt.size > 0);
+                av_packet_unref(&orig_pkt);}
+            else
+            {
+                pthread_mutex_lock(m_mutex);
+                m_ctx->state=END_OF_DECODING;
+                pthread_mutex_unlock(m_mutex);
+                break;
+            }
         }
-    }
-    else{
-        //if(log_level)
-        //printf("Input Buffer overflow !!! \n");
-    }
+        else{
+            //if(log_level)
+            //printf("Input Buffer overflow !!! \n");
+        }
     }
 
     /* the function must return something - NULL will do */
