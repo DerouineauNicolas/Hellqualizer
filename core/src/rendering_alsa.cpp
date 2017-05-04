f4821b62ec099383e4a754ef47284ab03c26b66d
NicolasDEROUINEAU <nicolas.derouineau@telecom-paristech.fr>
2017-05-04 17:37:38 +0200

    First draft for alsa

--- core/src/rendering_alsa.cpp
+++ core/src/rendering_alsa.cpp
@@ -1,20 +1,18 @@
 #include <rendering.h>
-#include <ao/ao.h>
+#include <alsa/asoundlib.h>
 #include <unistd.h>
 
-static ao_device *device;
-static ao_sample_format ao_format;
-static int default_driver;
-static const int buffer_size=AVCODEC_MAX_AUDIO_FRAME_SIZE+ FF_INPUT_BUFFER_PADDING_SIZE;
+static int err;
+static short buf[128];
+static snd_pcm_t *playback_handle;
+static snd_pcm_hw_params_t *hw_params;
 
 /*############################LIBABO############*/
 
 Rendering::Rendering( HQ_Context *ctx){
 
-    //LIBAO INIT
+    /*
     ao_initialize();
-
-    /* -- Setup for default driver -- */
 
     default_driver = ao_default_driver_id();
 
@@ -26,10 +24,60 @@ Rendering::Rendering( HQ_Context *ctx){
     ao_format.byte_format = AO_FMT_NATIVE;
     ao_format.matrix=0;
 
-    /* -- Open driver -- */
-    device = ao_open_live(default_driver, &ao_format, NULL /* no options */);
+
+    device = ao_open_live(default_driver, &ao_format, NULL  no options );
     if (device == NULL) {
         fprintf(stderr, "Error opening device.\n");
+    }*/
+
+
+    if ((err = snd_pcm_open (&playback_handle, "default", SND_PCM_STREAM_PLAYBACK, 0)) < 0) {
+        fprintf (stderr, "cannot open audio device %s (%s)\n");
+        exit (1);
+    }
+
+    if ((err = snd_pcm_hw_params_malloc (&hw_params)) < 0) {
+        fprintf (stderr, "cannot allocate hardware parameter structure (%s)\n",
+                 snd_strerror (err));
+        exit (1);
+    }
+
+    if ((err = snd_pcm_hw_params_any (playback_handle, hw_params)) < 0) {
+        fprintf (stderr, "cannot initialize hardware parameter structure (%s)\n",
+                 snd_strerror (err));
+        exit (1);
+    }
+
+    if ((err = snd_pcm_hw_params_set_access (playback_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
+        fprintf (stderr, "cannot set access type (%s)\n",
+                 snd_strerror (err));
+        exit (1);
+    }
+
+    if ((err = snd_pcm_hw_params_set_format (playback_handle, hw_params, SND_PCM_FORMAT_S16_LE)) < 0) {
+        fprintf (stderr, "cannot set sample format (%s)\n",
+                 snd_strerror (err));
+        exit (1);
+    }
+
+    if ((err = snd_pcm_hw_params_set_channels (playback_handle, hw_params, ctx->channels)) < 0) {
+        fprintf (stderr, "cannot set channel count (%s)\n",
+                 snd_strerror (err));
+        exit (1);
+    }
+
+    if ((err = snd_pcm_hw_params (playback_handle, hw_params)) < 0) {
+        fprintf (stderr, "cannot set parameters (%s)\n",
+                 snd_strerror (err));
+        exit (1);
+    }
+
+    snd_pcm_hw_params_free (hw_params);
+
+    if ((err = snd_pcm_prepare (playback_handle)) < 0) {
+        fprintf (stderr, "cannot prepare audio interface for use (%s)\n",
+                 snd_strerror (err));
+        exit (1);
     }
 
     m_mutex=&ctx->m_mutex_decode_to_process;
@@ -40,8 +88,8 @@ Rendering::Rendering( HQ_Context *ctx){
 }
 
 Rendering::~Rendering(){
-        ao_close(device);
-        ao_shutdown();
+    /*ao_close(device);
+    ao_shutdown();*/
 }
 
 void *Rendering::play_thread(void *x_void_ptr)
@@ -66,7 +114,14 @@ void *Rendering::play_thread(void *x_void_ptr)
                 break;
             m_buffer_decode_process->Read(samples,output_size);
             processor->process(&samples,output_size, m_ctx);
-            ao_play(device,(char*)samples, output_size);
+            for (int i = 0; i < output_size; ++i) {
+                if ((err = snd_pcm_writei (playback_handle, samples, 128)) != 128) {
+                    fprintf (stderr, "write to audio interface failed (%s)\n",
+                             snd_strerror (err));
+                    exit (1);
+                }
+            }
+            //ao_play(device,(char*)samples, output_size);
             pthread_mutex_unlock(m_mutex);
         }
         else{
@@ -81,5 +136,5 @@ void *Rendering::play_thread(void *x_void_ptr)
 }
 
 void Rendering::InternalThreadEntry(){
-   this->play_thread(NULL);
+    this->play_thread(NULL);
 }
