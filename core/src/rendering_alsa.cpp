543071ff9cbe791221f840a4031dcb0f416e4bbd
NicolasDEROUINEAU <nicolas.derouineau@telecom-paristech.fr>
2017-05-10 15:35:50 +0200

    Alsa code cleaning

--- core/src/rendering_alsa.cpp
+++ core/src/rendering_alsa.cpp
@@ -3,18 +3,13 @@
 #include <unistd.h>
 
 static int err;
-static short buf[128];
-static snd_pcm_t *playback_handle;
 snd_pcm_hw_params_t *hwparams;
 snd_pcm_sw_params_t *swparams;
-static char *device = "plughw:0,0";                     /* playback device */
 static snd_pcm_format_t format = SND_PCM_FORMAT_S16;    /* sample format */
 static unsigned int rate = 44100;                       /* stream rate */
 static unsigned int channels = 2;                       /* count of channels */
 static unsigned int buffer_time = 500000;               /* ring buffer length in us */
 static unsigned int period_time = 100000;               /* period time in us */
-static double freq = 440;                               /* sinusoidal wave frequency in Hz */
-static int verbose = 0;                                 /* verbose flag */
 static int resample = 1;                                /* enable alsa-lib resampling */
 static int period_event = 0;                            /* produce poll event after each period */
 static snd_pcm_sframes_t buffer_size;
@@ -196,69 +191,6 @@ Rendering::Rendering( HQ_Context *ctx){
 
 }
 
-static void generate_sine(const snd_pcm_channel_area_t *areas,
-                          snd_pcm_uframes_t offset,
-                          int count, double *_phase)
-{
-        static double max_phase = 2. * M_PI;
-        double phase = *_phase;
-        double step = max_phase*freq/(double)rate;
-        unsigned char *samples_out[channels];
-        int steps[channels];
-        unsigned int chn;
-        int format_bits = snd_pcm_format_width(format);
-        unsigned int maxval = (1 << (format_bits - 1)) - 1;
-        int bps = format_bits / 8;  /* bytes per sample */
-        int phys_bps = snd_pcm_format_physical_width(format) / 8;
-        int big_endian = snd_pcm_format_big_endian(format) == 1;
-        int to_unsigned = snd_pcm_format_unsigned(format) == 1;
-        int is_float = (format == SND_PCM_FORMAT_FLOAT_LE ||
-                        format == SND_PCM_FORMAT_FLOAT_BE);
-        /* verify and prepare the contents of areas */
-        for (chn = 0; chn < channels; chn++) {
-                if ((areas[chn].first % 8) != 0) {
-                        printf("areas[%i].first == %i, aborting...\n", chn, areas[chn].first);
-                        exit(EXIT_FAILURE);
-                }
-                samples_out[chn] = /*(signed short *)*/(((unsigned char *)areas[chn].addr) + (areas[chn].first / 8));
-                if ((areas[chn].step % 16) != 0) {
-                        printf("areas[%i].step == %i, aborting...\n", chn, areas[chn].step);
-                        exit(EXIT_FAILURE);
-                }
-                steps[chn] = areas[chn].step / 8;
-                samples_out[chn] += offset * steps[chn];
-        }
-        /* fill the channel areas */
-        while (count-- > 0) {
-                union {
-                        float f;
-                        int i;
-                } fval;
-                int res, i;
-                if (is_float) {
-                        fval.f = sin(phase);
-                        res = fval.i;
-                } else
-                        res = sin(phase) * maxval;
-                if (to_unsigned)
-                        res ^= 1U << (format_bits - 1);
-                for (chn = 0; chn < channels; chn++) {
-                        /* Generate data in native endian format */
-                        if (big_endian) {
-                                for (i = 0; i < bps; i++)
-                                        *(samples_out[chn] + phys_bps - 1 - i) = (res >> i * 8) & 0xff;
-                        } else {
-                                for (i = 0; i < bps; i++)
-                                        *(samples_out[chn] + i) = (res >>  i * 8) & 0xff;
-                        }
-                        samples_out[chn] += steps[chn];
-                }
-                phase += step;
-                if (phase >= max_phase)
-                        phase -= max_phase;
-        }
-        *_phase = phase;
-}
 
 Rendering::~Rendering(){
     /*ao_close(device);
@@ -276,26 +208,11 @@ void *Rendering::play_thread(void *x_void_ptr)
     samples=(uint8_t*)malloc(buffer_size*sizeof(uint8_t));
 
     signed short *samples_out;
-    samples_out = (short int*)malloc((period_size * channels * snd_pcm_format_physical_width(format)) / 8);
 
-    areas = (snd_pcm_channel_area_t*)calloc(channels, sizeof(snd_pcm_channel_area_t));
-    if (areas == NULL) {
-            printf("No enough memory\n");
-            exit(EXIT_FAILURE);
-    }
-    for (chn = 0; chn < channels; chn++) {
-            areas[chn].addr = samples_out;
-            areas[chn].first = chn * snd_pcm_format_physical_width(format);
-            areas[chn].step = channels * snd_pcm_format_physical_width(format);
-    }
-
-    double phase = 0;
-    signed short *ptr;
-    int err, cptr;
+    int err;
 
     while(1){
         if(m_ctx->state==PLAY){
-#if 1
             pthread_mutex_lock(m_mutex);
             //printf("RENDER: %d \n",m_buffer_decode_process->GetReadAvail());
             while(m_buffer_decode_process->GetReadAvail()<output_size){
@@ -308,40 +225,15 @@ void *Rendering::play_thread(void *x_void_ptr)
             m_buffer_decode_process->Read(samples,output_size);
             processor->process(&samples,output_size, m_ctx);
             samples_out=(signed short*)samples;
-//            for(int i=0;i<(output_size/4);i++)
-//               printf("%d \n",*(samples_out+i));
+            //            for(int i=0;i<(output_size/4);i++)
+            //               printf("%d \n",*(samples_out+i));
 
-                if ((err = snd_pcm_writei (handle, samples_out, output_size/4)) != (output_size/4)) {
-                    fprintf (stderr, "write to audio interface failed (%s)\n",
-                             snd_strerror (err));
-                    exit (1);
-                }
-            //}
-            //ao_play(device,(char*)samples, output_size);
-            pthread_mutex_unlock(m_mutex);
-#endif
-
-#if 0
-            generate_sine(areas, 0, period_size, &phase);
-            ptr = samples_out;
-            cptr = period_size;
-            while (cptr > 0) {
-                    err = snd_pcm_writei(handle, ptr, cptr);
-                    for(int i=0;i<cptr;i++)
-                        printf("%d \n",*(ptr+i));
-//                    if (err == -EAGAIN)
-//                            continue;
-//                    if (err < 0) {
-//                            if (xrun_recovery(handle, err) < 0) {
-//                                    printf("Write error: %s\n", snd_strerror(err));
-//                                    exit(EXIT_FAILURE);
-//                            }
-//                            break;  /* skip one period */
-//                    }
-                    ptr += err * channels;
-                    cptr -= err;
+            if ((err = snd_pcm_writei (handle, samples_out, output_size/4)) != (output_size/4)) {
+                fprintf (stderr, "write to audio interface failed (%s)\n",
+                         snd_strerror (err));
+                exit (1);
             }
-#endif
+            pthread_mutex_unlock(m_mutex);
         }
         else{
             usleep(1000000);
