a522659fe30103cf019dae0bf40a2a9de2529bc7
NicolasDEROUINEAU <nicolas.derouineau@telecom-paristech.fr>
2017-05-22 12:04:18 +0200

    Force compatibility with older libav version + enable alsa default rendering

--- core/src/demuxing_decoding.cpp
+++ core/src/demuxing_decoding.cpp
@@ -81,42 +81,43 @@ int DemuxDecode::open_codec_context(int *stream_idx, AVCodecContext **dec_ctx, A
 
     ret = av_find_best_stream(fmt_ctx, type, -1, -1, NULL, 0);
     if (ret < 0) {
-        fprintf(stderr, "Could not find %s stream in input file '%s'\n",
-                av_get_media_type_string(type), m_src_filename);
+        /*fprintf(stderr, "Could not find %s stream in input file '%s'\n",
+                av_get_media_type_string(type), m_src_filename);*/
+        printf("Could not find %s stream in input file");
         return ret;
     } else {
         stream_index = ret;
         st = fmt_ctx->streams[stream_index];
 
         /* find decoder for the stream */
-        dec = avcodec_find_decoder(st->codecpar->codec_id);
+        dec = avcodec_find_decoder(st->codec->codec_id);
         if (!dec) {
-            fprintf(stderr, "Failed to find %s codec\n",
-                    av_get_media_type_string(type));
+            fprintf(stderr, "Failed to find codec\n");//,
+                    //av_get_media_type_string(type));
             return AVERROR(EINVAL);
         }
 
         /* Allocate a codec context for the decoder */
         *dec_ctx = avcodec_alloc_context3(dec);
         if (!*dec_ctx) {
-            fprintf(stderr, "Failed to allocate the %s codec context\n",
-                    av_get_media_type_string(type));
+            fprintf(stderr, "Failed to allocate the codec context\n");//,
+                   // av_get_media_type_string(type));
             return AVERROR(ENOMEM);
         }
 
-        /* Copy codec parameters from input stream to output codec context */
-        if ((ret = avcodec_parameters_to_context(*dec_ctx, st->codecpar)) < 0) {
-            fprintf(stderr, "Failed to copy %s codec parameters to decoder context\n",
-                    av_get_media_type_string(type));
-            return ret;
-        }
+//        /* Copy codec parameters from input stream to output codec context */
+//        if ((ret = avcodec_parameters_to_context(*dec_ctx, st->codec)) < 0) {
+//            fprintf(stderr, "Failed to copy %s codec parameters to decoder context\n");//,
+//                    //av_get_media_type_string(type));
+//            return ret;
+//        }
 
 
         /* Init the decoders, with or without reference counting */
         av_dict_set(&opts, "refcounted_frames", refcount ? "1" : "0", 0);
         if ((ret = avcodec_open2(*dec_ctx, dec, &opts)) < 0) {
-            fprintf(stderr, "Failed to open %s codec\n",
-                    av_get_media_type_string(type));
+            fprintf(stderr, "Failed to open codec\n");//,
+                   // av_get_media_type_string(type));
             return ret;
         }
         *stream_idx = stream_index;
