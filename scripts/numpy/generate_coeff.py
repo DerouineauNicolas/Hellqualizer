221948c8b53a2db1d37931ee19b34cdd99aa0d0e
nderouineau <nicolas.derouineau@irdeto.com>
2018-08-22 14:02:41 +0200

    Update script

--- scripts/numpy/generate_coeff.py
+++ scripts/numpy/generate_coeff.py
@@ -2,17 +2,17 @@ from scipy import signal
 import matplotlib.pyplot as plt
 import numpy as np
 
+Fs=44100;
 
-
-b, a = signal.cheby2(4, 40, 100, 'low', analog=True)
+b, a = signal.cheby2(4, 40, float(2000)/float(Fs), 'low', analog=False)
 w, h = signal.freqs(b, a)
-plt.semilogx(w, 20 * np.log10(abs(h)))
+plt.plot(w, 20 * np.log10(abs(h)))
 plt.title('Chebyshev Type II frequency response (rs=40)')
 plt.xlabel('Frequency [radians / second]')
 plt.ylabel('Amplitude [dB]')
 plt.margins(0, 0.1)
 plt.grid(which='both', axis='both')
-plt.axvline(100, color='green') # cutoff frequency
+plt.axvline((2*2000)/Fs, color='green') # cutoff frequency
 plt.axhline(-40, color='green') # rs
 plt.show()
 
