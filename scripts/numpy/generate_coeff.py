da859409df08d164a0793b562d63de4a92d3e5e7
Nicolas DEROUINEAU <nicodero72@gmail.com>
2018-09-25 09:40:04 +0200

    Update Script for IIR coeff

--- scripts/numpy/generate_coeff.py
+++ scripts/numpy/generate_coeff.py
@@ -2,19 +2,21 @@ from scipy import signal
 import matplotlib.pyplot as plt
 import numpy as np
 
-Fs=44100;
+Fs=48000
 
-b, a = signal.cheby2(2, 40, float(2000)/float(Fs), 'low', analog=False)
-w, h = signal.freqs(b, a)
-print (b , a)
-plt.plot(w, 20 * np.log10(abs(h)))
-plt.title('Chebyshev Type II frequency response (rs=40)')
-plt.xlabel('Frequency [radians / second]')
-plt.ylabel('Amplitude [dB]')
-plt.margins(0, 0.1)
-plt.grid(which='both', axis='both')
-plt.axvline((2*2000)/Fs, color='green') # cutoff frequency
-plt.axhline(-40, color='green') # rs
+b, a = signal.iirfilter(1, [0.2, 0.4], rs=60, btype='band',
+                        analog=False, ftype='cheby2')
+w, h = signal.freqz(b, a, 1000)
+print (b,a)
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+plt.plot(w, 20 * np.log10(abs(h)), 'b')
+plt.ylabel('Amplitude [dB]', color='b')
+plt.xlabel('Frequency [rad/sample]')
+ax2 = ax1.twinx()
+angles = np.unwrap(np.angle(h))
+plt.plot(w, angles, 'g')
+plt.ylabel('Angle (radians)', color='g')
+plt.grid()
+plt.axis('tight')
 plt.show()
-
-
